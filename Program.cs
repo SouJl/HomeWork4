9960fbfd0ccb047212c3553c3e45ba391efbe241
Akexandr <meln.alex97@gmail.com>
2022-05-05 00:26:39 +0700

    Предварительная реализация 3 задания

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace HomeWork4
 {
     class MyStaticArray
     {
-        public static int GetPairCount(int length, int pairValue) 
+        public static int GetPairCount(int length, int pairValue)
         {
             Random rnd = new Random();
             int[] arr = new int[length];
@@ -17,11 +17,11 @@ namespace HomeWork4
             for (int i = 0; i < arr.Length; i++)
             {
                 arr[i] = rnd.Next(-10000, 10000);
-                
+
                 if (i == arr.Length / 2)
                     Console.WriteLine();
                 Console.Write($"{arr[i]}\t");
-                
+
                 if (i > 0 && i < arr.Length - 1)
                 {
                     if (arr[i - 1] % pairValue == 0 && arr[i] % pairValue == 0)
@@ -38,8 +38,8 @@ namespace HomeWork4
         {
             try
             {
-                List<int> result = new List<int>();
                 StreamReader fs = new StreamReader(filename);
+                List<int> result = new List<int>();
                 while (!fs.EndOfStream)
                 {
                     result.Add(int.Parse(fs.ReadLine()));
@@ -47,12 +47,71 @@ namespace HomeWork4
                 fs.Close();
                 return result.ToArray();
             }
-            catch(IOException ex)
+            catch (IOException ex)
             {
                 Console.WriteLine(ex.Message);
                 return null;
             }
         }
+    }
+
+    class MyCoolArray
+    {
+        private int[] _arr;
+
+        public int this[int ndx]
+        {
+            get => _arr[ndx];
+            set
+            {
+                _arr[ndx] = value;
+            }
+        }
+        
+        public MyCoolArray(int length, int start, int offset)
+        {
+            _arr = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                _arr[i] = start + offset * (i + 1);
+            }
+        }
+
+        public int Sum
+        {
+            get
+            {
+                int sum = 0;
+                for (int i = 0; i < _arr.Length; i++)
+                {
+                    sum += _arr[i];
+                }
+                return sum;
+            }
+        }
+
+
+        public int[] Inverse
+        {
+            get
+            {
+                int[] result = new int[_arr.Length];
+                for (int i = 0; i < _arr.Length; i++)
+                {
+                    result[i] = -_arr[i];
+                }
+                return result;
+            }
+        }
+
+        public void Multi(int x)
+        {
+            for (int i = 0; i < _arr.Length; i++)
+            {
+                _arr[i] *= x; 
+            }
+        }
+
     }
 
     class Program
@@ -61,12 +120,15 @@ namespace HomeWork4
         {
             Console.WriteLine("Работа метода");
             SetArray();
-            
+
             Console.SetCursorPosition(0, 5);
-            
+
             Console.WriteLine("Работа класса");
             Console.WriteLine($"Найденое число пар элементов массива: {MyStaticArray.GetPairCount(20, 3)}");
             MyStaticArray.ReadArrayFromFile(@"C:\Users\Alexander\Desktop\array.txt");
+
+            MyCoolArray coolArray = new MyCoolArray(10, 0, 2);
+
             Console.ReadKey();
         }
 
@@ -79,7 +141,7 @@ namespace HomeWork4
             for (int i = 0; i < mass.Length; i++)
             {
                 mass[i] = rnd.Next(-10000, 10000);
-                
+
                 if (i == mass.Length / 2)
                     Console.WriteLine();
 
